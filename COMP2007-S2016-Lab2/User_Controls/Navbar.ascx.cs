1161d3861cc1af686a97000fbbe14edb1d4e2cb6
sakib717 <sakibsabbir7@gmail.com>
2016-05-27 21:30:30 -0400

    Added navbar user control

---
+++ COMP2007-S2016-Lab2/User_Controls/Navbar.ascx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/**
+ * @author: Sakib Patel- 200307608
+ * @date: 27th May 2016
+ * @version: 0.0.1 added the SetActivePage Method.
+ */
+
+namespace COMP2007_S2016_Lab2
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+        
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
